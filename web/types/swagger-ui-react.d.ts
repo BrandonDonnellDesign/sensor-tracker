a423abe75d11346092d0bded6cb3a6c416a20aaf
Bmdonnell <bmdonnell@gmail.com>
2025-11-05 19:45:38 -0800

    docs(web): update web documentation

---
+++ web/types/swagger-ui-react.d.ts
@@ -0,0 +1,24 @@
+declare module 'swagger-ui-react' {
+  import { ComponentType } from 'react';
+
+  interface SwaggerUIProps {
+    spec?: any;
+    url?: string;
+    docExpansion?: 'list' | 'full' | 'none';
+    defaultModelsExpandDepth?: number;
+    defaultModelExpandDepth?: number;
+    displayOperationId?: boolean;
+    displayRequestDuration?: boolean;
+    filter?: boolean | string;
+    showExtensions?: boolean;
+    showCommonExtensions?: boolean;
+    tryItOutEnabled?: boolean;
+    requestInterceptor?: (request: any) => any;
+    responseInterceptor?: (response: any) => any;
+    onComplete?: (system: any) => void;
+    [key: string]: any;
+  }
+
+  const SwaggerUI: ComponentType<SwaggerUIProps>;
+  export default SwaggerUI;
+}
